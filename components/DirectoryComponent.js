255809d124904aa073df3d57ffd30c1900655610
Anik Thomas <aniek.sw@gmail.com>
2021-05-12 16:23:55 -0600

    Components Part-2

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -5,9 +5,10 @@ import { ListItem } from 'react-native-elements';
 function Directory(props) {
     const renderDirectoryItem = ({item}) => {
         return (
-            <ListItem
+            <ListItem 
                 title={item.name}
                 subtitle={item.description}
+                onPress={() => props.onPress(item.id)}
                 leftAvatar={{ source: require('./images/react-lake.jpg')}}
             />
         );
