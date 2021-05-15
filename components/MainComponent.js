6496d19613a24390f97b21f41aeb469550f63f00
Anik Thomas <aniek.sw@gmail.com>
2021-05-15 12:16:01 -0600

    React-Native Week1 Project

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
+import About from './AboutComponent';
+import Contact from './ContactComponent';
 import { View, Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
+
 // import ExpoStatusBar from 'expo-status-bar/build/ExpoStatusBar';
 import ExpoConstants from 'expo-constants';
 //Expo constant: provide system infomation that remain constant throughout thelife time of app installation
@@ -50,10 +53,47 @@ const HomeNavigator = createStackNavigator(
         }
     }
 );
+
+const AboutNavigator = createStackNavigator(
+    {
+        About: { screen: About}
+    },
+    {
+        defaultNavigationOptions:{
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const ContactNavigator = createStackNavigator(
+    {
+        Contact: { screen: Contact}
+    },
+    {
+        defaultNavigationOptions:{
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
 const MainNavigator = createDrawerNavigator(
     {
         Home: { screen: HomeNavigator },
-        Directory: { screen: DirectoryNavigator }
+        Directory: { screen: DirectoryNavigator },
+        About: { screen: AboutNavigator},
+        Contact: { screen: ContactNavigator}
     },
     {
         drawerBackgroundColor: '#CEC8FF'
